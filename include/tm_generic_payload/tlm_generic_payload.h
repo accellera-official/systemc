b74a6ba155f8877422dde644e441fa14275d08b2
Charles Wilson <charles.wilson@eslx.com>
2007-10-08 18:49:28 +0000

    - added directory and base include for GP (tlm_generic_payload.h) and udpated tlm.h to include it

--- include/tm_generic_payload/tlm_generic_payload.h
+++ include/tm_generic_payload/tlm_generic_payload.h
@@ -18,31 +18,11 @@
  
 #pragma once
 
-#ifndef TLM_HEADER
-#define TLM_HEADER
+#if ( ! def ( __TLM_GENERIC_PAYLOAD_H ) )
+#define __TLM_GENERIC_PAYLOAD_H
 
-#include "systemc.h"  /// main SystemC header
-#include <deque>      /// STD deque class
-#include <map>        /// STD map class
-
-#include "tlm_core/tlm_core.h"
-#include "tlm_analysis/tlm_analysis.h"
-#include "tlm_annotated/tlm_annotated.h"
-#include "tlm_bus/tlm_bus.h"
-#include "tlm_generic_payload/tlm_generic_payload.h"
-//#include "tlm_analysis/tlm_bus_analysis.h"
-#include "tlm_ports/tlm_ports.h"
-#include "tlm_quantum/tlm_quantum.h"
-
-namespace tlm  /// TLM namespace
+namespace tlm_generic_payload  /// TLM generic payload
 {
-  using namespace tlm_core;
-  using namespace analysis;
-  using namespace tlm_annotated;
-  using namespace tlm_bus;
-  using namespace tlm_generic_payload;
-  using namespace tlm_ports;
-  using namespace tlm_quantum;
 }
 
-#endif /* TLM_HEADER */
+#endif /* __TLM_GENERIC_PAYLOAD_H */
