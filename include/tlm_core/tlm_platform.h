dedb2d9ba34d8f7d06b1c3507694f37c4d8fd513
Charles Wilson <charles.wilson@eslx.com>
2007-10-08 18:42:03 +0000

    - split platform specific code from tlm_core.h into tlm_platform.h (new)

--- include/tlm_core/tlm_platform.h
+++ include/tlm_core/tlm_platform.h
@@ -15,35 +15,27 @@
   language governing rights and limitations under the License.
 
  *****************************************************************************/
+ 
 #pragma once
 
-#ifndef TLM_CORE_HEADER
-#define TLM_CORE_HEADER
+#if ( ! defined ( __TLM_PLATFORM_H__ ) )
+#define __TLM_PLATFORM_H__
 
-//#include "systemc.h"
-#include "tlm_platform.h"
+#if ( ! defined ( _MSC_VER ) )
+#include <stdint.h>			/// standard scalar data types
+#endif /* ! _MSC_VER */
 
-namespace tlm_annotated  /// TLM annotated namespace
-{
-  template< typename T > class tlm_annotated_fifo;
-}
+// make sure we have the standard scalar data types
 
-namespace tlm_core  /// TLM core namespace
-{
+// 64-bit ints
+#if ( ! defined ( _INT64_T ) )
+#define _INT64_T
+typedef long long   int64_t;
+#endif /* ! _INT64_T */
 
-// The unannotated TLM interfaces
+#if ( ! defined ( _UINT64_T ) )
+#define _UINT64_T
+typedef unsigned long long   uint64_t;
+#endif /* ! _UINT64_T */
 
-#include "tlm_interfaces/tlm_core_ifs.h"
-#include "tlm_interfaces/tlm_master_slave_ifs.h"
-
-// The channels : tlm_fifo, tlm_transport_channel and tlm_req_rsp_channel
-
-#include "tlm_req_rsp/tlm_req_rsp_channels.h"
-
-// Some non blocking ports to provide static sensitivity
-
-#include "tlm_event_finder/tlm_nonblocking_port.h"
-
-}
-
-#endif /* TLM_CORE_HEADER */
+#endif /* __TLM_PLATFORM_H__ */
