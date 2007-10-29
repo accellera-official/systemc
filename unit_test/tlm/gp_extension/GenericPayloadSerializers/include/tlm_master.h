df79bf2dddc3f6ce5859efba39b456d7770df959
Gino Van Hauwermeiren <gino.van.hauwermeiren@nxp.com>
2007-10-29 23:51:24 +0000

    Updated to use the new tlm_generic_payload, rev 146

--- unit_test/tlm/gp_extension/GenericPayloadSerializers/include/tlm_master.h
+++ unit_test/tlm/gp_extension/GenericPayloadSerializers/include/tlm_master.h
@@ -48,47 +48,20 @@ public:
 
     void main()
 	{
-		// Test 256-bit access
-		prepare_arrays< sc_biguint<256> >(256, reg256);
-		write<256>(0,aux_data);
-		read<256>(0,rd_data);
-		compare_arrays(256);
+		std::cout << std::endl;
+		std::cout << " ###### TEST SERIALIZERS ###### " << std::endl; 
+		test_without_be();
+		std::cout << std::endl;
 
-		// Test 128-bit access 
-		prepare_arrays< sc_bigint<128> >(128, reg128);
-		write<128>(0,aux_data);
-		read<128>(0,rd_data);
-		compare_arrays(128);
+		std::cout << std::endl;
+		std::cout << " ###### TEST SERIALIZERS WITH BYTE-ENABLES ###### " << std::endl; 
+		test_with_be();
+		std::cout << std::endl;
 
-		// Test 64-bit access 
-		prepare_arrays< sc_uint<64> >(64, reg64);
-		write<64>(0,aux_data);
-		read<64>(0,rd_data);
-		compare_arrays(64);
-
- 		// Test 48-bit access 
-		prepare_arrays< sc_int<48> >(48, reg48);
-		write<48>(0,aux_data);
-		read<48>(0,rd_data);
-		compare_arrays(48);
-
- 		// Test 32-bit access 
-		prepare_arrays< unsigned int >(32, reg32);
-		write<32>(0,aux_data);
-		read<32>(0,rd_data);
-		compare_arrays(32);
-
- 		// Test 16-bit access 
-		prepare_arrays< short >(16, reg16);
-		write<16>(0,aux_data);
-		read<16>(0,rd_data);
-		compare_arrays(16);
-
- 		// Test 8-bit access 
-		prepare_arrays< unsigned char >(8, reg8);
-		write<8>(0,aux_data);
-		read<8>(0,rd_data);
-		compare_arrays(8);
+		std::cout << std::endl;
+		std::cout << " ###### TEST SERIALIZERS WITH CUSTOMIZED BYTE-ENABLES ###### " << std::endl; 
+		test_customized_be();
+		std::cout << std::endl;
 
 		sc_stop();
         sc_core::wait(SC_ZERO_TIME);
@@ -99,7 +72,11 @@ public:
 
 private:
 
-    tlm::tlm_generic_payload  m_gp;
+	void test_without_be();
+	void test_with_be();
+	void test_customized_be();
+	
+	tlm::tlm_generic_payload  m_gp;
 
 	sc_biguint<256> reg256;
 	sc_bigint<128>	reg128;
@@ -112,70 +89,51 @@ private:
 	unsigned char *wr_data; 
 	unsigned char *aux_data; 
 	unsigned char *rd_data; 
+	bool          *be;
+	unsigned int   be_length;
 
 	template< class T >
-	void prepare_arrays(int nbits, T& reg);
+	void prepare_arrays(int nbits, T& reg, bool shift = false);
+
+	template< int W >
+	void write(unsigned int address, unsigned char *data, bool* be = 0, unsigned int be_length = 0);
+	template< int W >
+	void read(unsigned int address, unsigned char *data, bool* be = 0, unsigned int be_length = 0);
 
 	void compare_arrays(int nbits);
-
-    //
-    // Read data to address (data in host endianness)
-    //
-	template< int W >
-	void write(unsigned int address, unsigned char *data);
- 	//
-    // Read data from address (returns data in host endianness)
-    //
-	template< int W >
-	void read(unsigned int address, unsigned char *data);
-
 
 };
 
 template< class T >
-void tlm_master::prepare_arrays(int nbits, T& reg) 
+void tlm_master::prepare_arrays(int nbits, T& reg, bool shift) 
 {
 	wr_data = new unsigned char [nbits/8]; 
 	aux_data = new unsigned char [nbits/8]; 
-	rd_data = new unsigned char [nbits/8]; 
+	rd_data = new unsigned char [nbits/8];
+	be = new bool [nbits/8];
+	be_length = nbits/8;
 
 	// Initialiaze arrays
 	for(int i=0;i<nbits/8;i++)
 	{
-		wr_data[i] = i;	aux_data[i] = 0; rd_data[i] = 0;
+		if(shift) {
+			wr_data[i] = i << 4;	aux_data[i] = 0; rd_data[i] = 0; be[i] = true;
+		} else {
+			wr_data[i] = i;	aux_data[i] = 0; rd_data[i] = 0; be[i] = true;
+		}
 	}
 
 	// test serializers in the initiator 
-	tlm::copy_from_array< T >(reg,0,wr_data);
-	tlm::copy_to_array< T >(reg,0,aux_data);
+	tlm::copy_from_array< T >(reg, 0, wr_data, 0, 0, tlm::TLM_LITTLE_ENDIAN);
+	tlm::copy_to_array< T >(reg, 0, aux_data, 0, 0, tlm::TLM_LITTLE_ENDIAN);
 }
 
-void tlm_master::compare_arrays(int nbits) 
-{
-	// check if everything went ok
-	bool error_flag = false;
-
-	for(int i=0;i<nbits/8;i++)
-	{
-		if(aux_data[i]!= rd_data[i])
-			error_flag =true;
-	}
-
-	if(error_flag)
-		std::cout << name() << " : read and write " << nbits << "-bit operation : ERROR " << std::endl;
-	else
-		std::cout << name() << " : read and write " << nbits << "-bit operation : OK  " << std::endl;
-
-	delete [] wr_data;	wr_data = 0;
-	delete [] aux_data;	aux_data = 0; 
-	delete [] rd_data;	rd_data = 0;
-}
-    
+   
 //
 // Read data to address (data in host endianness)
 //
 template< int W >
-void tlm_master::write(unsigned int address, unsigned char *data)
+void tlm_master::write(unsigned int address, unsigned char *data, bool* be, unsigned int be_length)
 {
 	// Single WRITE transaction 
 
@@ -184,8 +142,10 @@ void tlm_master::write(unsigned int address, unsigned char *data)
 	m_gp.set_command(tlm::TLM_WRITE_COMMAND);
 	m_gp.set_address(address);
 	m_gp.set_data_ptr(data);
-	m_gp.set_length(W/8);
-	
+	m_gp.set_data_length(W/8);
+	m_gp.set_byte_enable_ptr(be);
+	m_gp.set_byte_enable_length(be_length);
+
 	bus_port->nb_transport(&m_gp);
 	
 	if(m_gp.is_response_ok())
@@ -202,7 +162,7 @@ void tlm_master::write(unsigned int address, unsigned char *data)
 // Read data from address (returns data in host endianness)
 //
 template< int W >
-void tlm_master::read(unsigned int address, unsigned char *data)
+void tlm_master::read(unsigned int address, unsigned char *data, bool* be, unsigned int be_length)
 {
 	// Single READ transaction 
 	std::cout << name() << " : Single READ transaction : "; 
@@ -210,7 +170,9 @@ void tlm_master::read(unsigned int address, unsigned char *data)
 	m_gp.set_command(tlm::TLM_READ_COMMAND);
 	m_gp.set_address(address);
 	m_gp.set_data_ptr(data);
-	m_gp.set_length(W/8);
+	m_gp.set_data_length(W/8);
+	m_gp.set_byte_enable_ptr(be);
+	m_gp.set_byte_enable_length(be_length);
 	
 	bus_port->nb_transport(&m_gp);
 	
