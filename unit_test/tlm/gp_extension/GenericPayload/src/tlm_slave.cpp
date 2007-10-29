0901c984c22f882c450fe8aa334becc542ceffac
Gino Van Hauwermeiren <gino.van.hauwermeiren@nxp.com>
2007-10-29 23:45:29 +0000

    Updated to use the new tlm_generic_payload, rev 146

--- unit_test/tlm/gp_extension/GenericPayload/src/tlm_slave.cpp
+++ unit_test/tlm/gp_extension/GenericPayload/src/tlm_slave.cpp
@@ -36,6 +36,7 @@ tlm_slave::tlm_slave(sc_core::sc_module_name _name,
 	m_incr_address = bus_port.getBusDataWidth()/8; // bus data width in bytes
 
 	m_checker.burst_not_supported();
+	m_checker.byte_enable_not_supported();
 }
 
 void tlm_slave::nb_transport(tlm::tlm_generic_payload* gp)
@@ -48,17 +49,17 @@ void tlm_slave::nb_transport(tlm::tlm_generic_payload* gp)
 		unsigned int addr = (unsigned int)gp->get_address() - m_start_address;
         unsigned char* data = gp->get_data_ptr();
 
-		if(gp->get_command() == tlm::TLM_WRITE_COMMAND)
+		if(gp->is_write())
 		{
 			// single write transaction 
-			m_mem.write(data,addr,gp->get_length());
-			m_response_status = tlm::TLM_OK_RESP;
+			m_mem.write(data,addr,gp->get_data_length());
+			m_response_status = tlm::TLM_OK_RESPONSE;
 		}
 		else // TLM_READ_COMMAND
 		{
 			// single read transaction 
-			m_mem.read(data,addr,gp->get_length());
-			m_response_status = tlm::TLM_OK_RESP;
+			m_mem.read(data,addr,gp->get_data_length());
+			m_response_status = tlm::TLM_OK_RESPONSE;
 		}
 	}
 	else
