fef413ad29afce263d34a51ed88edaf450bdff8d
Charles Wilson <charles.wilson@eslx.com>
2007-12-10 21:00:26 +0000

    - added an additional master and slave to common_tlm_1 (examples)

---
+++ examples/tlm/common_tlm_1/include/tlm_1_master_2.h
@@ -0,0 +1,53 @@
+/*****************************************************************************
+
+  The following code is derived, directly or indirectly, from the SystemC
+  source code Copyright (c) 1996-2004 by all Contributors.
+  All Rights reserved.
+
+  The contents of this file are subject to the restrictions and limitations
+  set forth in the SystemC Open Source License Version 2.4 (the "License");
+  You may not use this file except in compliance with such restrictions and
+  limitations. You may obtain instructions on how to receive a copy of the
+  License at http://www.systemc.org/. Software distributed by Contributors
+  under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+  ANY KIND, either express or implied. See the License for the specific
+  language governing rights and limitations under the License.
+
+ *****************************************************************************/
+
+/*****************************************************************************
+  Original Authors:
+    Bill Bunton, ESLX
+    Charles Wilson, ESLX
+*****************************************************************************/
+
+#ifndef MASTER_HEADER
+#define MASTER_HEADER
+
+#include <systemc.h>
+
+//#include "bus_types.h"
+#include "basic_initiator_port.h"   // from tlm 1
+
+
+typedef unsigned int ADDRESS_TYPE;
+typedef unsigned int DATA_TYPE;
+
+using basic_protocol::basic_initiator_port;
+
+class tlm_1_master_2 : public sc_module
+{
+public:
+  tlm_1_master_2( sc_module_name module_name );
+
+  SC_HAS_PROCESS( tlm_1_master_2 );
+
+  basic_initiator_port<ADDRESS_TYPE,DATA_TYPE> initiator_port;
+
+private:
+  void run();
+  
+};
+
+
+#endif
