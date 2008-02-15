535c3d957adfde380e8b6f827180e30f13982c95
Charles Wilson <charles.wilson@eslx.com>
2008-02-15 00:21:27 +0000

    - corrected an issue where the address range checking was not being done properly in lt_target_memory.cpp (examples common/src) issue 69

--- examples/tlm/common/src/lt_target_memory.cpp
+++ examples/tlm/common/src/lt_target_memory.cpp
@@ -17,11 +17,11 @@
 /*==============================================================================
 
   @file lt_target_memory.cpp
-
+  
   @brief Implementation of lt_target_memory class
-
+  
   @Details
-    Implements the lt_target_memory class. This class does not support memory refresh or
+    Implements the lt_target_memory class. This class does not support memory refresh or 
     DMI.
 
   @Note
@@ -47,12 +47,12 @@ static const char *report_source = "lt_target_memory";
 
 /*==============================================================================
   @fn lt_target_memory
-
+  
   @brief class constructor
-
+  
   @details
     This is the class constructor.
-
+    
   @param module_name module name
   @param base_address memory's base address
   @param memory_size memory's size in bytes
@@ -62,15 +62,15 @@ static const char *report_source = "lt_target_memory";
   @param write_clocks memory's write delay
   @param refresh_clocks memory's refresh delay
   @param refresh_rate memory's refresh frequency
-
+  
   @retval void
-
+  
   @note
     1. memory is initialized to 0
 ==============================================================================*/
 
 lt_target_memory::lt_target_memory             ///< constructor
-   (  sc_core::sc_module_name  module_name     ///< module name
+   (  sc_core::sc_module_name  module_name     ///< module name 
     , sc_dt::uint64   base_address             ///< memory base address 64-bit
     , sc_dt::uint64   memory_size              ///< memory size 64-bit
     , unsigned int    memory_width             ///< memory width (bytes)(2, 4, 8, 16)
@@ -91,24 +91,24 @@ lt_target_memory::lt_target_memory             ///< constructor
    {
       // Allocate an array for the target's memory
        m_memory = new unsigned char[size_t(memory_size)];
-
+      
       // clear the memory
       memset(m_memory, 0, memory_width * size_t(memory_size));
-
+      
       // Bind the socket's export to the interface
       m_memory_socket(*this);
    }
-
+     
 /*==============================================================================
   @fn ~lt_target_memory
-
+  
   @brief class destructor
-
+  
   @details
     This routine handles the class cleanup.
-
+    
   @param void
-
+  
   @retval void
 ==============================================================================*/
 
@@ -120,22 +120,22 @@ lt_target_memory::~lt_target_memory(void)      ///< destructor
 
 /*==============================================================================
   @fn nb_transport
-
+  
   @brief non-blocking transport
-
+  
   @details
-
+  
   @param gp generic payload pointer
   @param phase transaction phase
   @param delay_time event processing time
-
+  
   @retval synchronization state
-
+  
   @note
     1. write overruns are ignored silently
     2. reads are preinitialized to 0
     3. read overruns are ignored
-
+    
 ==============================================================================*/
 
 tlm::tlm_sync_enum                              ///< synchronization state
@@ -145,7 +145,7 @@ lt_target_memory::nb_transport(                 ///< non-blocking transport
   , sc_core::sc_time         &delay_time)       ///< time it should take for transport
 {
   // Access the required attributes from the payload
-
+  
   sc_dt::uint64      address = gp.get_address();         ///< memory address
   tlm::tlm_command   command = gp.get_command();         ///< memory command
   unsigned char      *data   = gp.get_data_ptr();        ///< data pointer
@@ -155,7 +155,7 @@ lt_target_memory::nb_transport(                 ///< non-blocking transport
 
   // Error checking
   if (phase != tlm::BEGIN_REQ) {
-    REPORT_FATAL(report_source, "nb_transport", "phase not BEGIN_REQ");
+    REPORT_FATAL(report_source, __FUNCTION__, "phase not BEGIN_REQ");
   }
 
   // Perform the requested operation
@@ -163,10 +163,10 @@ lt_target_memory::nb_transport(                 ///< non-blocking transport
   {
     default:
     {
-      REPORT_FATAL(report_source, "nb_transport", "invalid command");
+      REPORT_FATAL(report_source, __FUNCTION__, "invalid command");
       break;
     }
-
+      
     case tlm::TLM_WRITE_COMMAND:
     {
       msg << "W -";
@@ -177,14 +177,14 @@ lt_target_memory::nb_transport(                 ///< non-blocking transport
       {
         msg << internal << setw( 2 ) << setfill( '0' ) << uppercase << hex << (int)data[i];
       }
-      REPORT_INFO(report_source, "nb_transport", msg.str());
-
+      REPORT_INFO(report_source, __FUNCTION__, msg.str());
+      
       delay_time += m_write_delay;
-
+      
       // global -> local address
       address -= m_base_address;
 
-      if  (  (address < m_base_address)
+      if  (  (address < 0)
           || (address >= m_memory_size))
       {
         // ignore out-of-bounds writes
@@ -197,28 +197,28 @@ lt_target_memory::nb_transport(                 ///< non-blocking transport
           {
             break;
           }
-
+          
           m_memory[address++] = data[i];
         }
       }
       break;
     }
-
+    
     case tlm::TLM_READ_COMMAND:
     {
       msg << "R -";
       msg << " A: 0x" << internal << setw( sizeof(address) * 2 ) << setfill( '0' ) << uppercase << hex << address;
       msg << " L: " << internal << setw( 2 ) << setfill( '0' ) << dec << length;
 
+      // clear read buffer
+      memset(data, 0, length);
+
       delay_time += m_read_delay;
 
       // global -> local address
       address -= m_base_address;
-
-      // clear read buffer
-      memset(data, 0, length);
-
-      if  (  (address < m_base_address)
+        
+      if  (  (address < 0)
           || (address >= m_memory_size))
       {
         // out-of-bounds read
@@ -234,13 +234,13 @@ lt_target_memory::nb_transport(                 ///< non-blocking transport
             // ignore out-of-bounds reads
             break;
           }
-
+          
           data[i] = m_memory[address++];
           msg << internal << setw( 2 ) << setfill( '0' ) << uppercase << hex << (int)data[i];
         }
       }
 
-      REPORT_INFO(report_source, "nb_transport", msg.str());
+      REPORT_INFO(report_source, __FUNCTION__, msg.str());
       break;
     }
   }
@@ -248,25 +248,25 @@ lt_target_memory::nb_transport(                 ///< non-blocking transport
   // Set parameters to indicate all is well
   phase = tlm::BEGIN_RESP;
   gp.set_response_status(tlm::TLM_OK_RESPONSE);
-  return result;
+  return result; 
 }
 
 /*==============================================================================
   @fn transport_dbg
-
+  
   @brief transport debug
-
+  
   @details
     This routine is not currently implemented
-
+  
   @param payload debug payload
-
+  
   @retval result code
-
+  
   @note
     1. not implemented
     2. currently generates a fatal report (OS: disabled)
-
+    
 ==============================================================================*/
 
 unsigned int                                ///< result
@@ -275,28 +275,28 @@ lt_target_memory::transport_dbg(            ///< transport debug
 )
 {
     // No error needed, disabled
-    //REPORT_FATAL(report_source, "transport_dbg", "routine not implemented");
+    //REPORT_FATAL(report_source, __FUNCTION__, "routine not implemented");
   return 0;
 }
 
 /*==============================================================================
   @fn get_direct_mem_ptr
-
+  
   @brief get direct memory pointer
-
+  
   @details
     This routine is not currently implemented.
-
+    
   @param address memory address
   @param dmi_mode read/write flag
   @param dmi_data data reference
-
+  
   @retval bool success / failure
-
+  
   @note
     1. not implemented
     2. currently generates a fatal report (OS: disabled)
-
+    
 ==============================================================================*/
 
 bool                                        ///< success / failure
@@ -308,6 +308,6 @@ lt_target_memory::get_direct_mem_ptr(       ///< get direct memory pointer
 {
     // It's perfectly allowed to not support DMI, no need to
     // throw an error:
-    // REPORT_FATAL(report_source, "get_direct_mem_ptr", "routine not implemented");
+    // REPORT_FATAL(report_source, __FUNCTION__, "routine not implemented");
     return false;
 }
