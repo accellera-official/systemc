12d0f6664e2c232ef6130c9c6facce49ed5b98ae
Charles Wilson <charles.wilson@eslx.com>
2008-05-14 22:32:45 +0000

    - corrected an issue with the DMI type enumeration using generic names causing conflicts on Windows with the MSVC compilers. this will break stuff.

--- include/tlm/tlm_h/tlm_trans/tlm_2_interfaces/tlm_dmi.h
+++ include/tlm/tlm_h/tlm_trans/tlm_2_interfaces/tlm_dmi.h
@@ -41,29 +41,35 @@ public:
   }
 // Enum for signaling the granted access type to the initiator. 
 // The initiator uses gp.m_command to indicate it intention (read/write)
-//  The target is allowed to promote READ or WRITE requests to READ_WRITE.
+//  The target is allowed to promote DMI_TYPE_READ or DMI_TYPE_WRITE
+//  requests to DMI_TYPE_READ_WRITE.
 
-  enum Type { IGNORE = 0x0, READ = 0x1, WRITE = 0x2, READ_WRITE = READ|WRITE };
+  enum dmi_type_e
+  { DMI_TYPE_IGNORE     = 0x00
+  , DMI_TYPE_READ       = 0x01
+  , DMI_TYPE_WRITE      = 0x02
+  , DMI_TYPE_READ_WRITE = DMI_TYPE_READ | DMI_TYPE_WRITE
+  };
   
   unsigned char*    get_dmi_ptr() const {return dmi_ptr;}
   sc_dt::uint64     get_start_address() const {return dmi_start_address;}
   sc_dt::uint64     get_end_address() const {return dmi_end_address;}
   sc_core::sc_time  get_read_latency() const {return dmi_read_latency;}
   sc_core::sc_time  get_write_latency() const {return dmi_write_latency;}
-  Type              get_granted_access() const {return dmi_type;}
-  bool              is_read_allowed() const {return (dmi_type & READ) != 0;}
-  bool              is_write_allowed() const {return (dmi_type & WRITE) != 0;}
-  bool              is_read_write_allowed() const {return (dmi_type & READ_WRITE) != 0;}
+  dmi_type_e        get_granted_access() const {return dmi_type;}
+  bool              is_read_allowed() const {return (dmi_type & DMI_TYPE_READ) != 0;}
+  bool              is_write_allowed() const {return (dmi_type & DMI_TYPE_WRITE) != 0;}
+  bool              is_read_write_allowed() const {return (dmi_type & DMI_TYPE_READ_WRITE) != 0;}
 
   void              set_dmi_ptr(unsigned char* p) {dmi_ptr = p;}
   void              set_start_address(sc_dt::uint64 addr) {dmi_start_address = addr;}
   void              set_end_address(sc_dt::uint64 addr) {dmi_end_address = addr;}
   void              set_read_latency(sc_core::sc_time t) {dmi_read_latency = t;}
   void              set_write_latency(sc_core::sc_time t) {dmi_write_latency = t;}
-  void              set_granted_access(Type t) {dmi_type = t;}
-  void              allow_read() {dmi_type = READ;}
-  void              allow_write() {dmi_type = WRITE;}
-  void              allow_read_write() {dmi_type = READ_WRITE;}
+  void              set_granted_access(dmi_type_e t) {dmi_type = t;}
+  void              allow_read() {dmi_type = DMI_TYPE_READ;}
+  void              allow_write() {dmi_type = DMI_TYPE_WRITE;}
+  void              allow_read_write() {dmi_type = DMI_TYPE_READ_WRITE;}
 
 private:
   // If the forward call is successful, the target returns the dmi_ptr,
@@ -80,7 +86,7 @@ private:
   sc_dt::uint64 dmi_end_address;
 
   // Granted access
-  Type dmi_type;
+  dmi_type_e dmi_type;
 
   // These members define the latency of read/write transactions. The
   // initiator must initialize these members to zero before requesting a
