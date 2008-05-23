7e0983ddd482361e53b49a953bebe8379338e97b
Bart Vanthournout <bart@coware.com>
2008-05-23 09:22:46 +0000

    updated Copyright window

--- unit_test/tlm/endian_conv/src/testall.py
+++ unit_test/tlm/endian_conv/src/testall.py
@@ -2,7 +2,7 @@
 /*****************************************************************************
 
   The following code is derived, directly or indirectly, from the SystemC
-  source code Copyright (c) 1996-2007 by all Contributors.
+  source code Copyright (c) 1996-2008 by all Contributors.
   All Rights reserved.
 
   The contents of this file are subject to the restrictions and limitations
--- unit_test/tlm/multi_sockets/include/simpleAddressMap.h
+++ unit_test/tlm/multi_sockets/include/simpleAddressMap.h
@@ -1,11 +1,19 @@
-/*
- *  simpleAddressMap.h
- *  tlm2.0
- *
- *  Created by Robert Günzel on 25.04.08.
- *  Copyright 2008 E.I.S.. All rights reserved.
- *
- */
+/*****************************************************************************
+
+  The following code is derived, directly or indirectly, from the SystemC
+  source code Copyright (c) 1996-2008 by all Contributors.
+  All Rights reserved.
+
+  The contents of this file are subject to the restrictions and limitations
+  set forth in the SystemC Open Source License Version 3.0 (the "License");
+  You may not use this file except in compliance with such restrictions and
+  limitations. You may obtain instructions on how to receive a copy of the
+  License at http://www.systemc.org/. Software distributed by Contributors
+  under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+  ANY KIND, either express or implied. See the License for the specific
+  language governing rights and limitations under the License.
+
+ *****************************************************************************/
 
 #ifndef __simpleAddressMap_h__
 #define __simpleAddressMap_h__
