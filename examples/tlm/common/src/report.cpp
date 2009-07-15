241f1d4f7f570bcacd1ddd3c92abb9fbcc99230e
John Aynsley <john.aynsley@doulos.com>
2009-07-15 10:29:52 +0000

    When printing out data, for consistency across LE and BE hosts the 4 bytes must be printed in MSB:LSB order rather than in byte address order. The previous situation had caused mis-matches in expected results between Intel and Sparc hosts.

--- examples/tlm/common/src/report.cpp
+++ examples/tlm/common/src/report.cpp
@@ -16,7 +16,7 @@
 ///  @file report.cpp
 //
 ///  @Details
-///   
+///
 //
 //=====================================================================
 //  Original Authors:
@@ -28,65 +28,65 @@
 //static const char *filename = "report.cpp"; ///< filename for reporting
 
 namespace report {
-std::string print(const tlm::tlm_phase phase) 
-{ 
+std::string print(const tlm::tlm_phase phase)
+{
   std::string os;
   switch (phase)
   {
-    case tlm::BEGIN_REQ:        
-      { 
+    case tlm::BEGIN_REQ:
+      {
         os =  "BEGIN_REQ";
         break;
       }
-    case tlm::END_REQ:        
-      { 
+    case tlm::END_REQ:
+      {
         os = "END_REQ";
         break;
-      }  
+      }
     case tlm::BEGIN_RESP:
-      { 
+      {
         os = "BEGIN_RESP";
         break;
       }
     case tlm::END_RESP:
-      { 
+      {
         os = "END_RESP";
         break;
       }
     default:
-      {     
+      {
 //        char buffer[8];
 //        snprintf(buffer,8," %d",int(phase));
 //        os += buffer;
-        os += "UNKNOWN: " + phase;        
+        os += "UNKNOWN: " + phase;
         break;
       }
   }//end switch
   return os;
 }
 
-std::string print(const tlm::tlm_sync_enum status) 
+std::string print(const tlm::tlm_sync_enum status)
 { //
   std::string os ( "TLM_" );
   switch (status)
   {
-    case tlm::TLM_COMPLETED:        
-      { 
+    case tlm::TLM_COMPLETED:
+      {
         os = "COMPLETED";
         break;
       }
-    case tlm::TLM_UPDATED:        
-      { 
+    case tlm::TLM_UPDATED:
+      {
         os = "UPDATED";
         break;
-      }  
+      }
     case tlm::TLM_ACCEPTED:
-      { 
+      {
         os = "ACCEPTED";
         break;
       }
     default:
-      { 
+      {
 //        char buffer[8];
 //        snprintf(buffer,8," %d",int(status));
 //        os += buffer;
@@ -97,9 +97,9 @@ std::string print(const tlm::tlm_sync_enum status)
   return os;
 }
 //=====================================================================
-//  
+//
 ///  @brief helper function for printing memory transactions
-//   
+//
 //=====================================================================
 void
 print
@@ -110,32 +110,33 @@ print
 {
   std::ostringstream     msg;
   msg.str("");
-  
+
   sc_dt::uint64 print_address  = gp.get_address();      // memory address
   unsigned char *print_data    = gp.get_data_ptr();    // data pointer
   unsigned int  print_length   = gp.get_data_length(); // data length
   tlm::tlm_command print_command = gp.get_command();     // memory command
-  
+
   msg << "ID: "<< ID
       << " COMMAND: "
       << ((print_command == tlm::TLM_WRITE_COMMAND) ? "WRITE" : "READ")
-      << " Length: " << internal << setw( 2 ) << setfill( '0' ) 
+      << " Length: " << internal << setw( 2 ) << setfill( '0' )
       << dec << print_length
       << endl
-      << "      Addr: 0x" << internal << setw( sizeof(print_address) * 2 ) 
-      << setfill( '0' ) << uppercase << hex << print_address  
+      << "      Addr: 0x" << internal << setw( sizeof(print_address) * 2 )
+      << setfill( '0' ) << uppercase << hex << print_address
       << " Data: 0x"
-      << internal << setw( 2 ) << setfill( '0' ) << uppercase << hex << (int)print_data[3]
-      << internal << setw( 2 ) << setfill( '0' ) << uppercase << hex << (int)print_data[2]
-      << internal << setw( 2 ) << setfill( '0' ) << uppercase << hex << (int)print_data[1]
-      << internal << setw( 2 ) << setfill( '0' ) << uppercase << hex << (int)print_data[0];
-  
+
+      // For consistency across hosts (LE/BE), must interpret word as MSB:LSB rather than
+      // a sequence of bytes in address order
+      << internal << setw( 8 ) << setfill( '0' ) << uppercase << hex
+      << *reinterpret_cast<unsigned int*>(print_data);
+
   REPORT_INFO(calling_filename, __FUNCTION__, msg.str());
 }
 //=====================================================================
-//  
+//
 ///  @brief helper function for printing memory transactions
-//   
+//
 //=====================================================================
 void
 print_full
@@ -146,19 +147,19 @@ print_full
 {
   std::ostringstream     msg;
   msg.str("");
-  
+
   sc_dt::uint64 print_address  = gp.get_address();      // memory address
   unsigned char *print_data    = gp.get_data_ptr();    // data pointer
   unsigned int  print_length   = gp.get_data_length(); // data length
   tlm::tlm_command print_command = gp.get_command();     // memory command
-  
+
   msg << "ID: "<< ID
       << " COMMAND: "
       << ((print_command == tlm::TLM_WRITE_COMMAND) ? "WRITE" : "READ")
       << " Length: " << internal << setw( 2 ) << setfill( '0' ) << dec << print_length
       << endl
-      << "      Addr: 0x" << internal << setw( sizeof(print_address) * 2 ) 
-      << setfill( '0' ) << uppercase << hex << print_address 
+      << "      Addr: 0x" << internal << setw( sizeof(print_address) * 2 )
+      << setfill( '0' ) << uppercase << hex << print_address
       << " Data: 0x"
       << internal << setw( 2 ) << setfill( '0' ) << uppercase << hex << (int)print_data[3]
       << internal << setw( 2 ) << setfill( '0' ) << uppercase << hex << (int)print_data[2]
@@ -172,37 +173,37 @@ print_full
       << "DMI ALLOWED = " << gp.is_dmi_allowed()
       << endl << "      "
       << "BYTE ENABLE LENGTH = " << gp.get_byte_enable_length();
-  
+
   REPORT_INFO(caller_filename, __FUNCTION__, msg.str());
 }
-void  
+void
 print
 ( const int                 &ID              ///< Target ID/Initiator/?ID
 , tlm::tlm_dmi              &dmi_properties     ///< dmi transaction to be printed
 , const char*               calling_filename
-)       
-{  
+)
+{
    std::ostringstream     msg;
    msg.str("");
-   msg << "Initiator: " << ID 
+   msg << "Initiator: " << ID
 //       << " &dmi_properties = " << &dmi_properties
        << endl << "      "
        << "start addr     = " << dmi_properties.get_start_address()
        << endl << "      "
-       << "end addr       = " << dmi_properties.get_end_address() 
+       << "end addr       = " << dmi_properties.get_end_address()
        << endl << "      "
        << "read latency   = " << dmi_properties.get_read_latency()
        << endl << "      "
-       << "write latency  = " << dmi_properties.get_write_latency() 
+       << "write latency  = " << dmi_properties.get_write_latency()
        << endl << "      "
        << "granted access = " << dmi_properties.get_granted_access();
-   
+
 //   unsigned char*    temp_dmi_ptr = dmi_properties.get_dmi_ptr();
 //   msg  << endl << "      "
-//        << "dmi_ptr = " << temp_dmi_ptr; 
-   
+//        << "dmi_ptr = " << temp_dmi_ptr;
+
    REPORT_INFO(calling_filename, __FUNCTION__, msg.str());
-   
-}     
-    
+
+}
+
 }//end namespace
