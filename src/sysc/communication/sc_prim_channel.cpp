d76c85cb9223a96282c08c74ca19c7f2b388c17d
Philipp A. Hartmann <philipp.hartmann@offis.de>
2011-04-18 13:42:01 +0200

    Imported systemc-2.3.0_20110415_beta
    Summary: Imported systemc-2.3.0_20110415_beta
    Keywords:
    
    Imported systemc-2.3.0_20110415_beta
    into Git repository

--- src/sysc/communication/sc_prim_channel.cpp
+++ src/sysc/communication/sc_prim_channel.cpp
@@ -224,8 +224,11 @@ sc_prim_channel_registry::remove( sc_prim_channel& prim_channel_ )
 // constructor
 
 sc_prim_channel_registry::sc_prim_channel_registry( sc_simcontext& simc_ )
-: m_simc( &simc_ ), m_update_list_p((sc_prim_channel*)sc_prim_channel::list_end)
+: m_simc( &simc_ ), 
+  m_safe_update_list_p((sc_prim_channel*)sc_prim_channel::list_end),
+  m_update_list_p((sc_prim_channel*)sc_prim_channel::list_end)
 {
+    SC_SCOPED_MUTEX_INIT(m_safe_update_mutex);
 }
 
 
