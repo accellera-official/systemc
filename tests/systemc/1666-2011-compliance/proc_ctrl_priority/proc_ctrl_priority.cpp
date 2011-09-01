f94889887ca027f8c3d0b7c5f10c0c8cb68fa2c6
Philipp A. Hartmann <philipp.hartmann@offis.de>
2011-09-01 11:28:26 +0200

    fix index typos

--- tests/systemc/1666-2011-compliance/proc_ctrl_priority/proc_ctrl_priority.cpp
+++ tests/systemc/1666-2011-compliance/proc_ctrl_priority/proc_ctrl_priority.cpp
@@ -47,7 +47,7 @@ struct Top: sc_module
 
     count = 0;
     f1 = f2 = f3 = f4 = f5 = f6 = f7 = f8 = f9 = 0;
-    f10 = f11 = f12 = f13 = f14 = f15 = f16 = f19 = f18 = f19 = 0;
+    f10 = f11 = f12 = f13 = f14 = f15 = f16 = f17 = f18 = f19 = 0;
     f20 = f21 = f22 = f23 = f24 = f25 = f26 = f27 = f28 = f29 = 0;
   }
   
