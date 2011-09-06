801c769496cb7e3cc46ed6932b58dc99ce66ab6b
Philipp A. Hartmann <philipp.hartmann@offis.de>
2011-09-06 11:15:39 +0200

    Imported systemc_tests-2.3.0_pub_rev_20110905
    Summary: Imported systemc_tests-2.3.0_pub_rev_20110905
    Keywords:
    
    Imported systemc_tests-2.3.0_pub_rev_20110905
    into Git repository

--- scripts/verify.pl
+++ scripts/verify.pl
@@ -566,11 +566,9 @@ sub init_globals
         $rt_debug_flag    = "-g";
 	$rt_optimize_flag = "-O3";
     } elsif( $rt_systemc_arch eq "gcchpux11" ) {
-        $rt_ccflags       = "-Aa -ext +DA2.0 +DS2.0";
-        $rt_optimize_flag = "+O1";
+        #use defaults
     } elsif( $rt_systemc_arch eq "hpux11" ) {
-       $rt_ccflags       = "-Aa -ext +DA2.0 +DS2.0";
-       $rt_optimize_flag = "+O1";
+        #use defaults
     } elsif( $rt_systemc_arch eq "linux64" ) {
 	# use defaults
     } elsif( $rt_systemc_arch eq "linux" ) {
