ff522c1e132fbc625659b9111237caf4ab81319a
Henning Kleen <henning.kleen@offis.de>
2011-09-07 16:07:46 +0200

    improve registration of main coroutine on windows
    This patch adds a check to see if call to 'ConvertThreadToFiber'
    was successful, otherwise the current thread has already been
    converted to a fiber.  In that case, use 'GetCurrentFiber' to
    initialise main_cor.m_fiber instead.

--- src/sysc/kernel/sc_cor_fiber.cpp
+++ src/sysc/kernel/sc_cor_fiber.cpp
@@ -88,9 +88,19 @@ sc_cor_pkg_fiber::sc_cor_pkg_fiber( sc_simcontext* simc )
 : sc_cor_pkg( simc )
 {
     if( ++ instance_count == 1 ) {
+
         // initialize the main coroutine
-	assert( main_cor.m_fiber == 0 );
-	main_cor.m_fiber = ConvertThreadToFiber( 0 );
+        assert( main_cor.m_fiber == 0 );
+        main_cor.m_fiber = ConvertThreadToFiber( 0 );
+
+        if( !main_cor.m_fiber && GetLastError() == ERROR_ALREADY_FIBER ) {
+            // conversion of current thread to fiber has failed, because
+            // someone else already converted the main thread to a fiber
+            // -> store current fiber
+            main_cor.m_fiber = GetCurrentFiber();
+        }
+        assert( main_cor.m_fiber != 0 );
+
 #       if defined(__GNUC__) && __USING_SJLJ_EXCEPTIONS__
             // initialize the current coroutine
             assert( curr_cor == 0 );
