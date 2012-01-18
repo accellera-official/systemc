c3197a098b71bd73a8ede7f00cdaf1b5662f3c98
Alan Fitch <alan.fitch@doulos.com>
2012-01-18 17:42:45 +0000

    Included the tlm unit_tests directory from TLM2 into the systemc regression suite. Removed all the original makefiles (no longer needed). Moved various include files into the .cpp directory, and removed include subdirectories in that case. Modifed the verify.pl scripts to expect TLM_HOME to be set, and added tlm includes to the compile commands. Modified golden files where appropriate (startup message SystemC Simulation at the head, INFO simulation stopped by user at the end). Renamed all log files to match verify.pl expectations.

--- tests/tlm/build-msvc/openhere.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-%~d1
-cd %~p1
-cd %1
-%SystemRoot%\system32\cmd.exe
---
+++ tests/tlm/p2p/CoreDecoupling/core_decoupling.cpp
@@ -0,0 +1,34 @@
+/*****************************************************************************
+
+  The following code is derived, directly or indirectly, from the SystemC
+  source code Copyright (c) 1996-2008 by all Contributors.
+  All Rights reserved.
+
+  The contents of this file are subject to the restrictions and limitations
+  set forth in the SystemC Open Source License Version 3.0 (the "License");
+  You may not use this file except in compliance with such restrictions and
+  limitations. You may obtain instructions on how to receive a copy of the
+  License at http://www.systemc.org/. Software distributed by Contributors
+  under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+  ANY KIND, either express or implied. See the License for the specific
+  language governing rights and limitations under the License.
+
+ *****************************************************************************/
+#define SC_INCLUDE_DYNAMIC_PROCESSES
+#include "tlm.h"
+
+#include "CoreDecouplingLTInitiator.h"
+#include "ExplicitLTTarget.h"
+
+int sc_main(int argc, char* argv[])
+{
+  CoreDecouplingLTInitiator initiator("initiator", 10);
+  ExplicitLTTarget target("target");
+
+  initiator.socket(target.socket);
+
+  sc_core::sc_start();
+  sc_core::sc_stop();
+
+  return 0;
+}
