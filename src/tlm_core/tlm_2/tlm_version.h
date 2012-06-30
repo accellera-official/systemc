cf0efb320fac9999225df2c55be012f7406975cc
Philipp A. Hartmann <philipp.hartmann@offis.de>
2012-06-30 16:10:12 +0200

    SystemC/TLM: Version originator -> ASI

--- src/tlm_core/tlm_2/tlm_version.h
+++ src/tlm_core/tlm_2/tlm_version.h
@@ -64,7 +64,7 @@ namespace tlm
 #define TLM_VERSION_MAJOR                   2           ///< version major level ( numeric )
 #define TLM_VERSION_MINOR                   0           ///< version minor level ( numeric )
 #define TLM_VERSION_PATCH                   2           ///< version patch level ( numeric )
-#define TLM_VERSION_ORIGINATOR              "ACCELLERA" ///< TLM creator string
+#define TLM_VERSION_ORIGINATOR              "ASI"       ///< TLM creator string
 #define TLM_VERSION_SEPARATOR               "."         ///< version string separator
 
 #define TLM_IS_PRERELEASE                   1           ///< pre-release flag ( 1 / 0 )
