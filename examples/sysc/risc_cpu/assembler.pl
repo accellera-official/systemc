0691666b887a3814c3b4b8d439a8b4dcb418dde5
Philipp A. Hartmann <philipp.hartmann@offis.de>
2013-12-15 19:10:02 +0100

    update copyright headers
     * drop version reference to SystemC Open Source License
     * switch to accellera.org
     * update copyright year (->2014 already)
    
    Updates done by the following shell snippet:
    
       grep -rl 'SystemC Open Source License' | \
        xargs -n 1 \
         sed -i -e 's/Version ... (/(/' \
                -e 's/www\.systemc\.org/www.accellera.org/' \
                -e 's/(c) \(....\)-.... by/(c) \1-2014 by/'
    
    See OSCI-WG/systemc#63.

--- examples/sysc/risc_cpu/assembler.pl
+++ examples/sysc/risc_cpu/assembler.pl
@@ -7,7 +7,7 @@
 #  Use License Version 1.0 (the "License"); you may not use this file except
 #  in compliance with such restrictions and limitations. You may obtain
 #  instructions on how to receive a copy of the License at
-#  http://www.systemc.org/. Software distributed by Original Contributor
+#  http://www.accellera.org/. Software distributed by Original Contributor
 #  under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
 #  ANY KIND, either express or implied. See the License for the specific
 #  language governing rights and limitations under the License.
