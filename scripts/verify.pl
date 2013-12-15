a566fc8802daa31fbdbeb6615102efff4661a7d7
Philipp A. Hartmann <philipp.hartmann@offis.de>
2013-12-15 18:14:01 +0100

    update copyright headers
     * update copyright years to include 2014
     * drop explicit license version from headers
     * systemc.org -> accellera.org
    
    Updates done by the following shell snippet:
    
       grep -rl 'SystemC Open Source License' | \
         xargs -n 1 \
           sed -i -e 's/Version ... (/(/' \
                  -e 's/www\.systemc\.org/www.accellera.org/' \
                  -e 's/(c) \(....\)-.... by/(c) \1-2014 by/'
    
    See OSCI-WG/systemc#63.

--- scripts/verify.pl
+++ scripts/verify.pl
@@ -8,14 +8,14 @@ eval '(exit $?0)' && eval 'exec perl -S $0 ${1+"$@"}'
 # *****************************************************************************
 #
 #  The following code is derived, directly or indirectly, from the SystemC
-#  source code Copyright (c) 1996-2012 by all Contributors.
+#  source code Copyright (c) 1996-2014 by all Contributors.
 #  All Rights reserved.
 #
 #  The contents of this file are subject to the restrictions and limitations
-#  set forth in the SystemC Open Source License Version 2.3 (the "License");
+#  set forth in the SystemC Open Source License (the "License");
 #  You may not use this file except in compliance with such restrictions and
 #  limitations. You may obtain instructions on how to receive a copy of the
-#  License at http://www.systemc.org/. Software distributed by Contributors
+#  License at http://www.accellera.org/. Software distributed by Contributors
 #  under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
 #  ANY KIND, either express or implied. See the License for the specific
 #  language governing rights and limitations under the License.
