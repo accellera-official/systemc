21706e5046255a2b4c9857ec7ec23df2a03cb3f7
Philipp A. Hartmann <philipp.hartmann@offis.de>
2014-11-30 13:57:26 +0100

    sc_ver.h: version bump (switch to 2.3.2_pub_rev)

--- src/sysc/kernel/sc_ver.h
+++ src/sysc/kernel/sc_ver.h
@@ -53,12 +53,12 @@ extern const std::string  sc_copyright_string;
 
 #define SYSTEMC_2_3_1
  
-#define SYSTEMC_VERSION       20140417
+#define SYSTEMC_VERSION       20141231
 #define SC_VERSION_ORIGINATOR "Accellera"
 #define SC_VERSION_MAJOR      2
 #define SC_VERSION_MINOR      3
-#define SC_VERSION_PATCH      1
-#define SC_IS_PRERELEASE      0
+#define SC_VERSION_PATCH      2
+#define SC_IS_PRERELEASE      1
 
 /// compliancy with IEEE 1666-2011 (see 8.6.5)
 #define IEEE_1666_SYSTEMC     201101L
