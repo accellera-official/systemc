f4dca3e6bc70835c096668fa0fc001cbcfaca3e4
Philipp A Hartmann <philipp.a.hartmann@intel.com>
2015-11-26 09:37:20 +0100

    tlm_phase: move extended phase registration to library
    This patch adds tlm_phase.cpp and a central mapping from
    the specialized tlm_phase type and the phase ID.  This
    mapping inside a (shared) library provides stable IDs
    even for dynamically loaded models defining additional
    phases.

--- src/tlm_core/tlm_2/tlm_generic_payload/tlm_phase.h
+++ src/tlm_core/tlm_2/tlm_generic_payload/tlm_phase.h
@@ -17,71 +17,92 @@
 
  *****************************************************************************/
 
-#ifndef __TLM_PHASE_H__
-#define __TLM_PHASE_H__
+#ifndef TLM_CORE_TLM2_TLM_PHASE_H_INCLUDED_
+#define TLM_CORE_TLM2_TLM_PHASE_H_INCLUDED_
 
 #include <string>
 #include <iostream>
+#include <typeinfo>
 #include <vector>
+
+#include "sysc/kernel/sc_cmnhdr.h" // SC_API
+#include "sysc/kernel/sc_macros.h" // SC_CONCAT_HELPER_, SC_STRINGIFY_HELPER_
 
 namespace tlm {
 
-//enum tlm_phase { BEGIN_REQ, END_REQ, BEGIN_RESP, END_RESP };
-  
-enum tlm_phase_enum { UNINITIALIZED_PHASE=0, BEGIN_REQ=1, END_REQ, BEGIN_RESP, END_RESP };
+enum SC_API tlm_phase_enum
+{
+  UNINITIALIZED_PHASE=0,
+  BEGIN_REQ=1,
+  END_REQ,
+  BEGIN_RESP,
+  END_RESP
+};
 
-inline unsigned int create_phase_number(){
-  static unsigned int number=END_RESP+1;
-  return number++;
-}
-
-inline std::vector<const char*>& get_phase_name_vec(){
-  static std::vector<const char*> phase_name_vec(END_RESP+1, (const char*)NULL);
-  return phase_name_vec;
-}
-
-class tlm_phase{
+class SC_API tlm_phase
+{
 public:
-  tlm_phase(): m_id(0) {}
-  tlm_phase(unsigned int id): m_id(id){}
-  tlm_phase(const tlm_phase_enum& standard): m_id((unsigned int) standard){}
-  tlm_phase& operator=(const tlm_phase_enum& standard){m_id=(unsigned int)standard; return *this;}
-  operator unsigned int() const{return m_id;}
-  
+  tlm_phase();
+  tlm_phase(unsigned int id); // TODO: should be dropped
+
+  tlm_phase(tlm_phase_enum standard);
+  tlm_phase& operator=(tlm_phase_enum standard);
+
+  operator unsigned int() const { return m_id; }
+  const char* get_name() const;
+
+protected:
+  // register extended phase
+  tlm_phase( const std::type_info & type, const char* name );
+
 private:
-  unsigned int m_id;  
+  unsigned int m_id;
 };
 
 inline
-std::ostream& operator<<(std::ostream& s, const tlm_phase& p){
-  switch ((unsigned int)p){
-    case UNINITIALIZED_PHASE: s<<"UNINITIALIZED_PHASE"; break;
-    case BEGIN_REQ:  s<<"BEGIN_REQ"; break;
-    case END_REQ:    s<<"END_REQ"; break;
-    case BEGIN_RESP: s<<"BEGIN_RESP"; break;
-    case END_RESP:   s<<"END_RESP"; break;
-    default:
-      s<<get_phase_name_vec()[(unsigned int)p]; return s;      
-  }
+tlm_phase::tlm_phase()
+  : m_id( UNINITIALIZED_PHASE )
+{}
+
+inline
+tlm_phase::tlm_phase( tlm_phase_enum standard )
+  : m_id( standard )
+{}
+
+inline
+tlm_phase& tlm_phase::operator=( tlm_phase_enum standard )
+{
+  m_id = standard;
+  return *this;
+}
+
+inline
+std::ostream& operator<<(std::ostream& s, const tlm_phase& p)
+{
+  s << p.get_name();
   return s;
 }
-  
+
 #define TLM_DECLARE_EXTENDED_PHASE(name_arg) \
-class tlm_phase_##name_arg:public tlm::tlm_phase{ \
-public:\
-static const tlm_phase_##name_arg& get_phase(){static tlm_phase_##name_arg tmp; return tmp;}\
-private:\
-tlm_phase_##name_arg():tlm::tlm_phase(tlm::create_phase_number()){tlm::get_phase_name_vec().push_back(get_char_##name_arg());};\
-tlm_phase_##name_arg(const tlm_phase_##name_arg&); \
-tlm_phase_##name_arg& operator=(const tlm_phase_##name_arg&); \
-static inline const char* get_char_##name_arg(){static const char* tmp=#name_arg; return tmp;} \
-}; \
-static const tlm_phase_##name_arg& name_arg=tlm_phase_##name_arg::get_phase()
+  static class SC_CONCAT_HELPER_(tlm_phase_, name_arg) \
+    : public ::tlm::tlm_phase \
+  { \
+    typedef SC_CONCAT_HELPER_(tlm_phase_, name_arg) this_type; \
+  public: \
+    SC_CONCAT_HELPER_(tlm_phase_, name_arg)() /* register extended phase */ \
+      : ::tlm::tlm_phase( typeid(*this), SC_STRINGIFY_HELPER_(name_arg) ) \
+    {} \
+    \
+    static const this_type& get_phase() /* needed only for IEEE 1666-2011 */ \
+      { static this_type this_; return this_; } \
+  } \
+  const name_arg
 
 // for backwards-compatibility
-#define DECLARE_EXTENDED_PHASE(NameArg) \
+#define DECLARE_EXTENDED_PHASE( NameArg ) \
     TLM_DECLARE_EXTENDED_PHASE( NameArg )
 
 } // namespace tlm
 
-#endif /* TLM_PHASE_HEADER */
+#endif /* TLM_CORE_TLM2_TLM_PHASE_H_INCLUDED_ */
+// Taf!
