4fdb91ad5e5fc891da9a1b02c1a1299f27d9cffc
Torsten Maehne <torsten.maehne@bfh.ch>
2016-09-04 23:50:55 +0200

    Delete trailing whitespace from the lt_synch_target implementation files.

--- examples/tlm/common/include/lt_synch_target.h
+++ examples/tlm/common/include/lt_synch_target.h
@@ -2,7 +2,7 @@
     The following code is derived, directly or indirectly, from the SystemC
     source code Copyright (c) 1996-2014 by all Contributors.
     All Rights reserved.
- 
+
     The contents of this file are subject to the restrictions and limitations
     set forth in the SystemC Open Source License (the "License");
     You may not use this file except in compliance with such restrictions and
@@ -24,21 +24,21 @@
 //   Charles Wilson, ESLX
 //=====================================================================
 
-#ifndef __LT_SYNCH_TARGET_H__ 
+#ifndef __LT_SYNCH_TARGET_H__
 #define __LT_SYNCH_TARGET_H__
 
 #include "tlm.h"                                // TLM headers
 #include "memory.h"
 #include "tlm_utils/simple_target_socket.h"
 
-class lt_synch_target                      
+class lt_synch_target
 :     public sc_core::sc_module           	/// inherit from SC module base clase
 //, virtual public tlm::tlm_fw_transport_if<>   /// inherit from TLM "forward interface"
 
 {
 // Member Methods =====================================================
-	
-  public:	  
+
+  public:
 //=====================================================================
 ///	@fn lt_synch_target
 ///
@@ -46,11 +46,11 @@ class lt_synch_target
 ///
 ///	@details
 ///		Generic Single Phase target used in several examples.
-///		Constructor offers several parameters for customization	
+///		Constructor offers several parameters for customization
 ///
 //=====================================================================
-  
-  lt_synch_target                          
+
+  lt_synch_target
   ( sc_core::sc_module_name   module_name           ///< SC module name
   , const unsigned int        ID                    ///< target ID
   , const char                *memory_socket        ///< socket name
@@ -60,26 +60,26 @@ class lt_synch_target
   , const sc_core::sc_time    read_response_delay   ///< read response delay (SC_TIME, SC_NS)
   , const sc_core::sc_time    write_response_delay  ///< write response delay (SC_TIME, SC_NS)
   );
- 
+
   private:
-	    
+
 /// b_transport() - Blocking Transport
-  void                                                // returns nothing 
+  void                                                // returns nothing
   custom_b_transport
-  ( tlm::tlm_generic_payload  &payload                // ref to payload 
-  , sc_core::sc_time          &delay_time             // delay time 
+  ( tlm::tlm_generic_payload  &payload                // ref to payload
+  , sc_core::sc_time          &delay_time             // delay time
   );
 
 // Member Variables ===================================================
-  
-  public:  
-	  
+
+  public:
+
   typedef tlm::tlm_generic_payload  *gp_ptr;		///< generic payload pointer
-  
+
   tlm_utils::simple_target_socket<lt_synch_target>  m_memory_socket; ///<  target socket
 
   private:
-	  
+
   const unsigned int        m_ID;                   ///< target ID
         sc_dt::uint64       m_memory_size;          ///< memory size (bytes)
         unsigned int        m_memory_width;         ///< word size (bytes)
