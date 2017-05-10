c0bd7d61da5aefc3064cb30358bb69a89d77f15b
Mohamed Bamakhrama <mohamed@users.noreply.github.com>
2017-05-10 17:33:35 +0200

    Update tlm_endian_conv.h

--- src/tlm_core/tlm_2/tlm_generic_payload/tlm_endian_conv.h
+++ src/tlm_core/tlm_2/tlm_generic_payload/tlm_endian_conv.h
@@ -289,27 +289,20 @@ inline void copy_db0(uchar *src1, uchar *src2, uchar *dest1, uchar *dest2) {
   *dest2 = *src2;
 }
 
-inline void copy_dbtrue0(uchar *src1, uchar *src2, uchar *dest1, uchar *dest2) {
-  (void) src2;
+inline void copy_dbtrue0(uchar *src1, uchar * /* src2 */, uchar *dest1, uchar *dest2) {
   *dest1 = *src1;
   *dest2 = TLM_BYTE_ENABLED;
 }
 
-inline void copy_btrue0(uchar *src1, uchar *src2, uchar *dest1, uchar *dest2) {
-  (void) src1;
-  (void) src2;
-  (void) dest1;
+inline void copy_btrue0(uchar * /* src1 */, uchar * /* src2 */, uchar * /* dest1 */, uchar *dest2) {
   *dest2 = TLM_BYTE_ENABLED;
 }
 
-inline void copy_b0(uchar *src1, uchar *src2, uchar *dest1, uchar *dest2) {
-  (void) src1;
-  (void) dest1;
+inline void copy_b0(uchar * /* src1 */, uchar *src2, uchar * /* dest1 */, uchar *dest2) {
   *dest2 = *src2;
 }
 
-inline void copy_dbyb0(uchar *src1, uchar *src2, uchar *dest1, uchar *dest2) {
-  (void) src2;
+inline void copy_dbyb0(uchar *src1, uchar * /* src2 */, uchar *dest1, uchar *dest2) {
   if(*dest2 == TLM_BYTE_ENABLED) *src1 = *dest1;
 }
 
