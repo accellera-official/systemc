5084cc1dd25418d30a63645b47418734a3f0a6c8
Philipp A. Hartmann <philipp.a.hartmann@intel.com>
2017-05-28 12:35:09 +0200

    sc_*_registry: fix warning on GCC 7.0
    GCC 7.0 emits the following warning due to a (theoretical) call to
    std::vector::resize(-1):
    
      In member function 'void sc_core::sc_module_registry::remove(sc_core::sc_module&)':
      cc1plus: warning: 'void* __builtin_memset(void*, int, long unsigned int)' specified size 18446744073709551608 exceeds maximum object size 9223372036854775807 [-Wwarning=stringop-overflow=]
    
    Use `back()` and `pop_back()` instead.

--- src/sysc/communication/sc_prim_channel.cpp
+++ src/sysc/communication/sc_prim_channel.cpp
@@ -238,8 +238,8 @@ sc_prim_channel_registry::remove( sc_prim_channel& prim_channel_ )
     }
 
     // remove
-    m_prim_channel_vec[i] = m_prim_channel_vec[size() - 1];
-    m_prim_channel_vec.resize(size()-1);
+    m_prim_channel_vec[i] = m_prim_channel_vec.back();
+    m_prim_channel_vec.pop_back();
 }
 
 bool
