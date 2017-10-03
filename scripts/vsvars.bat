2004114de98c600bcad375f451ec5bb37d185fe4
Philipp A Hartmann <philipp.a.hartmann@intel.com>
2017-10-03 15:27:23 +0200

    vsvars.bat: updates for MSVC 2017
     * Accept 14.1 and 15.0 as versions to select MSVC 2017
       - query VS141COMNTOOLS or VS150COMNTOOLS, respectively
     * Add a note, that these variables need to be set manually to use
       the script for MSVC 2017.
     * Prefix all internal variables with `SYSTEMC_`

--- scripts/vsvars.bat
+++ scripts/vsvars.bat
@@ -40,6 +40,9 @@ rem  The installation location for each version is determined by the
 rem      VS<major><minor>COMNTOOLS
 rem  environment variable (e.g. VS140COMNTOOLS for version 14.0/2015).
 rem
+rem  Note: Visual Studio 2017 no longer sets the VS<ver>COMNTOOLS variable
+rem        by default.  In order to use this helper script, you need to
+rem        set it manually in your environment.
 rem ***************************************************************************
 
 set SYSTEMC_MSVC_VERSION=
@@ -59,6 +62,7 @@ if "%1" == "2013"  goto check_MSVC120
 if "%1" == "2014"  goto check_MSVC120
 if "%1" == "14.0"  goto check_MSVC140
 if "%1" == "2015"  goto check_MSVC140
+if "%1" == "14.1"  goto check_MSVC141
 if "%1" == "15.0"  goto check_MSVC150
 if "%1" == "2017"  goto check_MSVC150
 
@@ -71,44 +75,57 @@ rem installation.  This should be usually the case by default.
 
 :check_MSVC80
 set SYSTEMC_MSVC_VERSION=8.0 (2005)
-set VSCOMNTOOLS=%VS80COMNTOOLS%
-set VSVERSHORT=80
+set SYSTEMC_VSCOMNTOOLS=%VS80COMNTOOLS%
+set SYSTEMC_VSVERSHORT=80
 goto load_MSVC
 
 :check_MSVC90
 set SYSTEMC_MSVC_VERSION=9.0 (2008)
-set VSCOMNTOOLS=%VS90COMNTOOLS%
-set VSVERSHORT=90
+set SYSTEMC_VSCOMNTOOLS=%VS90COMNTOOLS%
+set SYSTEMC_VSVERSHORT=90
 goto load_MSVC
 
 :check_MSVC100
 set SYSTEMC_MSVC_VERSION=10.0 (2010)
-set VSCOMNTOOLS=%VS100COMNTOOLS%
-set VSVERSHORT=100
+set SYSTEMC_VSCOMNTOOLS=%VS100COMNTOOLS%
+set SYSTEMC_VSVERSHORT=100
 goto load_MSVC
 
 :check_MSVC110
 set SYSTEMC_MSVC_VERSION=11.0 (2012)
-set VSCOMNTOOLS=%VS110COMNTOOLS%
-set VSVERSHORT=110
+set SYSTEMC_VSCOMNTOOLS=%VS110COMNTOOLS%
+set SYSTEMC_VSVERSHORT=110
 goto load_MSVC
 
 :check_MSVC120
 set SYSTEMC_MSVC_VERSION=12.0 (2013)
-set VSCOMNTOOLS=%VS120COMNTOOLS%
-set VSVERSHORT=120
+set SYSTEMC_VSCOMNTOOLS=%VS120COMNTOOLS%
+set SYSTEMC_VSVERSHORT=120
 goto load_MSVC
 
 :check_MSVC140
 set SYSTEMC_MSVC_VERSION=14.0 (2015)
-set VSCOMNTOOLS=%VS140COMNTOOLS%
-set VSVERSHORT=140
+set SYSTEMC_VSCOMNTOOLS=%VS140COMNTOOLS%
+set SYSTEMC_VSVERSHORT=140
+goto load_MSVC
+
+:check_MSVC141
+rem In MSVC 2017, PlatformToolset and IDE version diverged.
+rem  - report the PlatformToolset version here
+set SYSTEMC_MSVC_VERSION=14.1 (2017)
+rem  - we use the PlatformToolset instead of the IDE version for the arch identifier
+set SYSTEMC_VSCOMNTOOLS=%VS141COMNTOOLS%
+set SYSTEMC_VSVERSHORT=141
+rem try %VS150COMNTOOLS%, if %VS141COMNTOOLS% is not set
+if not exist "%SYSTEMC_VSCOMNTOOLS%vsdevcmd.bat" goto check_MSVC150
 goto load_MSVC
 
 :check_MSVC150
+rem In MSVC 2017, PlatformToolset and IDE version diverged.
+rem  - report the IDE version here
 set SYSTEMC_MSVC_VERSION=15.0 (2017)
-set VSCOMNTOOLS=%VS150COMNTOOLS%
-set VSVERSHORT=150
+set SYSTEMC_VSCOMNTOOLS=%VS150COMNTOOLS%
+set SYSTEMC_VSVERSHORT=150
 goto load_MSVC
 
 :load_MSVC
@@ -116,29 +133,28 @@ if "%SYSTEMC_MSVC_PLATFORM%" == "" set SYSTEMC_MSVC_PLATFORM=%2
 if "%SYSTEMC_MSVC_PLATFORM%" == "" set SYSTEMC_MSVC_PLATFORM=x86
 
 echo Platform = Microsoft Visual C++ %SYSTEMC_MSVC_VERSION% (%SYSTEMC_MSVC_PLATFORM% architecture)
-echo Location = "%VSCOMNTOOLS%" (VS%VSVERSHORT%COMNTOOLS)
+echo Location = "%SYSTEMC_VSCOMNTOOLS%" (VS%SYSTEMC_VSVERSHORT%COMNTOOLS)
 
-if not exist "%VSCOMNTOOLS%vsdevcmd.bat" goto load_MSVC_VARSALL
+if not exist "%SYSTEMC_VSCOMNTOOLS%vsdevcmd.bat" goto load_MSVC_VARSALL
 echo Loading vsdevcmd.bat for Microsoft Visual C++ %SYSTEMC_MSVC_VERSION% (%SYSTEMC_MSVC_PLATFORM% architecture)
-call "%VSCOMNTOOLS%vsdevcmd.bat" -arch=%SYSTEMC_MSVC_PLATFORM%
+call "%SYSTEMC_VSCOMNTOOLS%vsdevcmd.bat" -arch=%SYSTEMC_MSVC_PLATFORM%
 goto end
 
 rem Try locating and loading vsvarsall.bat manually
 :load_MSVC_VARSALL
-set VSINSTALLDIR=%VSCOMNTOOLS%..\..\
-if not exist "%VSINSTALLDIR%" goto error_no_MSVC_VERSION
-set VCINSTALLDIR=%VSINSTALLDIR%VC\
-set VCVARSALL=%VCINSTALLDIR%vcvarsall.bat
-if not exist "%VCVARSALL%" set VCVARSALL=%VCINSTALLDIR%Auxiliary\Build\vcvarsall.bat
-if not exist "%VCVARSALL%" goto error_no_MSVC_VERSION
+set SYSTEMC_VSINSTALLDIR=%SYSTEMC_VSCOMNTOOLS%..\..\
+if not exist "%SYSTEMC_VSINSTALLDIR%" goto error_no_MSVC_VERSION
+set SYSTEMC_VCINSTALLDIR=%SYSTEMC_VSINSTALLDIR%VC\
+set SYSTEMC_VCVARSALL=%SYSTEMC_VCINSTALLDIR%vcvarsall.bat
+if not exist "%SYSTEMC_VCVARSALL%" set SYSTEMC_VCVARSALL=%SYSTEMC_VCINSTALLDIR%Auxiliary\Build\vcvarsall.bat
+if not exist "%SYSTEMC_VCVARSALL%" goto error_no_MSVC_VERSION
 echo Loading vcvarsall.bat for Microsoft Visual C++ %SYSTEMC_MSVC_VERSION% (%SYSTEMC_MSVC_PLATFORM% architecture)
-call "%VCVARSALL%" %SYSTEMC_MSVC_PLATFORM%
+call "%SYSTEMC_VCVARSALL%" %SYSTEMC_MSVC_PLATFORM%
 goto end
 
 :error_no_MSVC_VERSION
 echo ERROR: Microsoft Visual C++ %SYSTEMC_MSVC_VERSION% not found. Environment not loaded.
 echo Please check your MS Visual C++ %SYSTEMC_MSVC_VERSION% installation, and make
-echo sure to set the VS%VSVERSHORT%COMNTOOLS environment variable.
-goto end
+echo sure to set the VS%SYSTEMC_VSVERSHORT%COMNTOOLS environment variable.
 
 :end
