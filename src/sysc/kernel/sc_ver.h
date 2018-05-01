54c46b2b3717bcbfab0a2170c8c8b72800c73223
Philipp A Hartmann <philipp.a.hartmann@intel.com>
2018-05-01 20:04:45 +0200

    Bump remaining version references
     * Bump SystemC copyright string to include 2018
     * Define SYSTEMC_2_3_3 (instead of SYSTEMC_2_3_2)
     * Bump TLM version to 2.0.5 (prerelease)
     * Update build system files to SystemC 2.3.3, TLM 2.0.5

--- src/sysc/kernel/sc_ver.h
+++ src/sysc/kernel/sc_ver.h
@@ -54,7 +54,7 @@ extern SC_API const bool         sc_is_prerelease;
 extern SC_API const std::string  sc_version_string;
 extern SC_API const std::string  sc_copyright_string;
 
-#define SYSTEMC_2_3_2
+#define SYSTEMC_2_3_3
 
 #define SYSTEMC_VERSION       20180415
 #define SC_VERSION_ORIGINATOR "Accellera"
@@ -67,7 +67,7 @@ extern SC_API const std::string  sc_copyright_string;
 #define IEEE_1666_SYSTEMC     201101L
 
 #define SC_COPYRIGHT                               \
-  "Copyright (c) 1996-2017 by all Contributors,\n" \
+  "Copyright (c) 1996-2018 by all Contributors,\n" \
   "ALL RIGHTS RESERVED\n"
 
 
