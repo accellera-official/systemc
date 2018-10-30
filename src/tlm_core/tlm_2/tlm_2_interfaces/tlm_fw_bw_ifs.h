265d6e842f9bf57a806e9b8d8de3a78784ff896c
Philipp A Hartmann <philipp.a.hartmann@intel.com>
2018-10-30 15:34:55 +0100

    tlm_base_*_socket: implement get_protocol_types()
     * Add "protocol_types" typedef to tlm_*_transport_if
     * Use protocol_types typedef to determine protocol types in
       socket base classes:
       - tlm_base_target_socket    -> FW_IF::protocol_types
       - tlm_base_initiator_socket -> BW_IF::protocol_types
    
    Closes #328.

--- src/tlm_core/tlm_2/tlm_2_interfaces/tlm_fw_bw_ifs.h
+++ src/tlm_core/tlm_2/tlm_2_interfaces/tlm_fw_bw_ifs.h
@@ -208,7 +208,10 @@ class tlm_fw_transport_if
   , public virtual tlm_blocking_transport_if<typename TYPES::tlm_payload_type>
   , public virtual tlm_fw_direct_mem_if<typename TYPES::tlm_payload_type>
   , public virtual tlm_transport_dbg_if<typename TYPES::tlm_payload_type>
-{};
+{
+public:
+  typedef TYPES protocol_types;
+};
 
 // The backward interface:
 template <typename TYPES = tlm_base_protocol_types>
@@ -216,7 +219,10 @@ class tlm_bw_transport_if
   : public virtual tlm_bw_nonblocking_transport_if<typename TYPES::tlm_payload_type,
                                                    typename TYPES::tlm_phase_type>
   , public virtual tlm_bw_direct_mem_if
-{};
+{
+public:
+  typedef TYPES protocol_types;
+};
 
 } // namespace tlm
 
--- src/tlm_core/tlm_2/tlm_sockets/tlm_target_socket.h
+++ src/tlm_core/tlm_2/tlm_sockets/tlm_target_socket.h
@@ -184,6 +184,8 @@ public:
     { return *this; }
   virtual unsigned int                    get_bus_width() const
     { return BUSWIDTH; }
+  virtual sc_core::sc_type_index          get_protocol_types() const
+    { return typeid(typename FW_IF::protocol_types); }
   virtual tlm_socket_category             get_socket_category() const
     { return TLM_TARGET_SOCKET; }
 
@@ -242,11 +244,6 @@ public:
   virtual const char* kind() const
   {
     return "tlm_target_socket";
-  }
-
-  virtual sc_core::sc_type_index get_protocol_types() const
-  {
-    return typeid(TYPES);
   }
 };
 
