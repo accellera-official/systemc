d45170a9232e6bd88dd1b80964db2f84891fec2c
Philipp A Hartmann <philipp.a.hartmann@intel.com>
2019-07-09 20:01:04 +0200

    sc_ver.h: bump version date

--- src/sysc/kernel/sc_ver.h
+++ src/sysc/kernel/sc_ver.h
@@ -56,7 +56,7 @@ extern SC_API const std::string  sc_copyright_string;
 
 #define SYSTEMC_2_4_0
 
-#define SYSTEMC_VERSION       20190517
+#define SYSTEMC_VERSION       20190709
 #define SC_VERSION_ORIGINATOR "Accellera"
 #define SC_VERSION_MAJOR      2
 #define SC_VERSION_MINOR      4
