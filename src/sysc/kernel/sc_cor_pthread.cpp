ca099e0d331b5bd8ccdcfe394ffc9e833fa6e418
Andy Goodrich <acg.node@gmail.com>
2021-03-19 15:28:12 -0400

    Andy Goodrich: add additional optional debugging messages.

--- src/sysc/kernel/sc_cor_pthread.cpp
+++ src/sysc/kernel/sc_cor_pthread.cpp
@@ -66,7 +66,6 @@ sc_cor_pthread::sc_cor_pthread()
     DEBUGF << this << ": sc_cor_pthread::sc_cor_pthread()" << std::endl;
     pthread_cond_init( &m_pt_condition, PTHREAD_NULL );
     pthread_mutex_init( &m_mutex, PTHREAD_NULL );
-    DEBUGF << this << ": exiting sc_cor_pthread::sc_cor_pthread()" << std::endl;
 }
 
 
@@ -77,7 +76,6 @@ sc_cor_pthread::~sc_cor_pthread()
     DEBUGF << this << ": sc_cor_pthread::~sc_cor_pthread()" << std::endl;
     pthread_cond_destroy( &m_pt_condition );
     pthread_mutex_destroy( &m_mutex );
-    DEBUGF << this << ": exiting sc_cor_pthread::~sc_cor_pthread()" << std::endl;
 }
 
 
@@ -145,10 +143,8 @@ sc_cor_pkg_pthread::sc_cor_pkg_pthread( sc_simcontext* simc )
 
 sc_cor_pkg_pthread::~sc_cor_pkg_pthread()
 {
-    DEBUGF << this << ": sc_cor_pkg_pthread::~sc_cor_pkg_pthread()" << std::endl;
     pthread_mutex_destroy(&m_create_mtx);
     pthread_cond_destroy(&m_create_cond);
-    DEBUGF << this << ": exiting sc_cor_pkg_pthread::~sc_cor_pkg_pthread()" << std::endl;
 }
 
 
