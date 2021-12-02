c4900eabf1ebe35e4afa1f95ac8b5dda503c4797
Andy Goodrich <acg.node@gmail.com>
2021-12-02 07:44:46 -0500

    Andy Goodrich: add Fred's configuration for the size of "small vec", the amount of storage present in sc_signed and sc_unsigned to short circuit dynamic allocation of storage for "small" values.

--- src/sysc/datatypes/int/sc_nbdefs.h
+++ src/sysc/datatypes/int/sc_nbdefs.h
@@ -131,7 +131,9 @@ namespace sc_dt
 // instance's value. The compile-time buffer's size is a trade-off between preventing malloc/free
 // invocations for the storage, and the footprint of sc_signed and sc_unsigned instances.
 
-#define SC_SMALL_VEC_DIGITS 40
+#ifndef SC_SMALL_VEC_DIGITS
+#   define SC_SMALL_VEC_DIGITS 4
+#endif
 
 typedef unsigned char uchar;
 
