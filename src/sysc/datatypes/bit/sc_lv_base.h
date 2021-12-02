543ba42bf64f13dd27605159c4e0444382147c4b
Andy Goodrich <acg.node@gmail.com>
2021-12-02 07:27:14 -0500

    Andy Goodrich: clean up merge issue.

--- src/sysc/datatypes/bit/sc_lv_base.h
+++ src/sysc/datatypes/bit/sc_lv_base.h
@@ -513,21 +513,12 @@ operator | ( const sc_proxy<X>& px, const sc_proxy<Y>& py )
     const X& x =  px.back_cast();
     const Y& y =  py.back_cast();
     if ( x.length() >= y.length() ) {
-<<<<<<< HEAD
 	sc_lv_base a( x );
 	return ( a |= y );
     }
     else {
 	sc_lv_base a( y );
 	return ( a |= x );
-=======
-        sc_lv_base a( x );
-        return ( a |= y );
-    }
-    else {
-        sc_lv_base a( y );
-        return ( a |= x );
->>>>>>> master
     }
 }
 
