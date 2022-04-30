3aaa1e3ed4f4c896c668d9f33eacb614129bae65
Andy Goodrich <acg.node@gmail.com>
2022-04-30 05:37:06 -0400

    Andy Goodrich: restore code that inadvertently got pushed.

--- src/sysc/communication/sc_prim_channel.h
+++ src/sysc/communication/sc_prim_channel.h
@@ -47,11 +47,13 @@ class SC_API sc_prim_channel
     friend class sc_prim_channel_registry;
 
 public:
+    enum { list_end = 0xdb };
+public:
     virtual const char* kind() const
         { return "sc_prim_channel"; }
 
     inline bool update_requested() 
-	{ return m_queued_for_update; }
+	{ return m_update_next_p != (sc_prim_channel*)list_end; }
 
     // request the update method to be executed during the update phase
     inline void request_update();
@@ -190,12 +192,6 @@ protected:
 	{ return sc_core::timed_out( simcontext() ); }
 
 
-#if 0 // @@@@####
-    // delta count maintenance
-    sc_dt::uint64 delta_count()
-	{ return simcontext()->m_delta_count; }
-#endif
-
 private:
 
     // called during the update phase of a delta cycle (if requested)
@@ -220,7 +216,7 @@ private:
 private:
 
     sc_prim_channel_registry* m_registry;          // Update list manager.
-    bool                      m_queued_for_update; // true if queued for update.
+    sc_prim_channel*          m_update_next_p;     // Next entry in update list.
 };
 
 
@@ -249,7 +245,8 @@ public:
 
     bool pending_updates() const
     { 
-        return m_update_list.size() != 0 || pending_async_updates();
+        return m_update_list_p != (sc_prim_channel*)sc_prim_channel::list_end 
+               || pending_async_updates();
     }   
 
     bool pending_async_updates() const;
@@ -303,7 +300,7 @@ private:
     int                           m_construction_done;   // # of constructs.
     std::vector<sc_prim_channel*> m_prim_channel_vec;    // existing channels.
     sc_simcontext*                m_simc;                // simulator context.
-    std::vector<sc_prim_channel*> m_update_list;         // list of internal updates.
+    sc_prim_channel*              m_update_list_p;       // internal updates.
 };
 
 
@@ -320,8 +317,8 @@ inline
 void
 sc_prim_channel_registry::request_update( sc_prim_channel& prim_channel_ )
 {
-    prim_channel_.m_queued_for_update = true;
-    m_update_list.push_back(&prim_channel_);
+    prim_channel_.m_update_next_p = m_update_list_p;
+    m_update_list_p = &prim_channel_;
 }
 
 // ----------------------------------------------------------------------------
@@ -336,7 +333,7 @@ inline
 void
 sc_prim_channel::request_update()
 {
-    if( !m_queued_for_update ) {
+    if( ! m_update_next_p ) {
 	m_registry->request_update( *this );
     }
 }
@@ -373,7 +370,7 @@ void
 sc_prim_channel::perform_update()
 {
     update();
-    m_queued_for_update = false;
+    m_update_next_p = 0;
 }
 
 
