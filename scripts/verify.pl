228d9e108a28af7d7b5296392acd886e5516abd1
Paul Ehrlich <paul.ehrlich@coseda-tech.com>
2022-05-02 14:48:39 +0200

    VS toolversion numbering - similar to SystemC AMS Regression
    Visual Studio tool version, then the numbering is 14 for Visual Studio
    2015, 15 for Visual Studio 2017, 16 for Visual Studio 2019 and 17 for
    Visual Studio 2022

--- scripts/verify.pl
+++ scripts/verify.pl
@@ -552,8 +552,17 @@ sub get_systemc_arch
                 elsif ( $v_string =~ /.+Version 18\.00/) {   # 2013
                     $arch = "msvc12";
                 }
-                elsif ( $v_string =~ /.+Version 19\.\d{2}/) {   # >2015
-                    $arch = "msvc14"; # reuse msvc14 identifier for now
+                elsif ( $v_string =~ /.+Version 19\.00/) {   # 2015
+                    $arch = "msvc14";
+                }
+                elsif ( $v_string =~ /.+Version 19\.1/) {   # 2017
+                    $arch = "msvc15"; 
+                }
+                elsif ( $v_string =~ /.+Version 19\.2/) {   # 2019
+                    $arch = "msvc16"; 
+                }
+                elsif ( $v_string =~ /.+Version 19\.3/) {   # 2022
+                    $arch = "msvc17"; 
                 }
                 else {
                     die "Error: unsupported compiler '$cxx' ($v_string)\n";
