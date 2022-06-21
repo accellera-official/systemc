cbde119a59c4f5b84835153e3ae16d4313b893a2
Laurent MAILLET-CONTOZ <59615009+lmailletcontoz@users.noreply.github.com>
2022-06-21 14:46:41 +0200

    Update sc_temporary.h
    added missing space in comment

--- src/sysc/utils/sc_temporary.h
+++ src/sysc/utils/sc_temporary.h
@@ -31,7 +31,7 @@
 #include "sysc/kernel/sc_cmnhdr.h"
 
 #include <cstddef>                // std::size_t
-#include <cstring>                //for memset
+#include <cstring>                // for memset
 
 namespace sc_core {
 
