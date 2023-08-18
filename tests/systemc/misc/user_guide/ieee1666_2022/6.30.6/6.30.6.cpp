5015544129151edd32034e238f9f7faea6ab218c
Andy Goodrich <acg.node@gmail.com>
2023-08-18 10:13:12 -0400

    Andy Goodrich: additional exampel tests.

---
+++ tests/systemc/misc/user_guide/ieee1666_2022/6.30.6/6.30.6.cpp
@@ -0,0 +1,33 @@
+#include <systemc.h>
+
+
+SC_MODULE(Mod) {
+    sc_core::sc_event_queue EQ;
+
+    SC_CTOR(Mod) {
+        SC_THREAD(T);
+	SC_METHOD(M);
+	sensitive << EQ;
+	dont_initialize();
+    }
+
+    void T() {
+        EQ.notify(2.0, sc_core::SC_NS);
+        EQ.notify(1.0, sc_core::SC_NS);
+	EQ.notify(sc_core::SC_ZERO_TIME);
+        EQ.notify(1.0, sc_core::SC_NS);
+    }
+
+    void M() {
+        std::cout << sc_time_stamp() << std::endl;
+    }
+};
+
+int sc_main( int argc, char* argv[] ) { 
+
+    Mod m("m");
+
+    sc_start();
+    std::cout << "program completed" << std::endl;
+    return 0;
+}
