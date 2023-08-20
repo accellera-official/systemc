8102e0eaa43f9ed2e6c5f19bbb7f91f844a702f0
Andy Goodrich <acg.node@gmail.com>
2023-08-20 14:25:30 -0400

    Andy Goodrich: remove sc_clock instance that was causing an infinite loop.

--- tests/systemc/misc/user_guide/ieee1666_2022/4.6.8/4.6.8.cpp
+++ tests/systemc/misc/user_guide/ieee1666_2022/4.6.8/4.6.8.cpp
@@ -15,37 +15,28 @@ SC_MODULE(TB)
 {
     SC_CTOR(TB)
     {
-        SC_CTHREAD(tb_thread,m_clk.pos());
+        SC_THREAD(tb_thread);
     }
     void tb_thread()
     {
-        for (size_t i = 0; true; ++i )
+        for (size_t i = 0; i < 10; ++i )
         {
 	    std::cout << " tb   " << i << "   " << sc_time_stamp() << std::endl;
-            wait();
+            wait(1.0, SC_NS);
         }
     }
-    sc_in<bool> m_clk;
 };
 
 int sc_main( int argc, char* argv[] ) {
 
-    sc_clock           clock;
     sc_signal<bool>    reset;
     TB                 tb("tb");
 
-    tb.m_clk(clock);
+    sc_start( SC_ZERO_TIME ); // Run the initialization phase to create pending activity
 
-  sc_start( SC_ZERO_TIME );                    // Run the initialization phase to create pending activity
-
-  for ( size_t i = 0; i < 56; ++i ) {
-      if ( sc_pending_activity() ) {
-          std::cout << " main " << i << "   "  << sc_time_stamp() << "   " 
-	            << sc_time_to_pending_activity() << std::endl;
-          sc_start( sc_time_to_pending_activity() ); // Run up to the next activity
-      }
-  }
-
-  return 0;
+    while ( sc_pending_activity() ) {
+        sc_start( sc_time_to_pending_activity() ); // Run up to the next activity
+    }
+    return 0;
 }
 
