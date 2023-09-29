b9de51d350339b77aed01777a7e0230fc4ac31ec
Andy Goodrich <acg.node@gmail.com>
2023-09-29 12:32:05 -0400

    Andy Goodrich: additional tests completed.

--- tests/systemc/misc/user_guide/ieee1666_2022/5.6.6.7/5.6.6.7.cpp
+++ tests/systemc/misc/user_guide/ieee1666_2022/5.6.6.7/5.6.6.7.cpp
@@ -1,19 +1,19 @@
-// NEEDS EXECUTION CODE
+// COMPLETE TEST
 //
-// At present this test will validate the compilation syntax, but needs an execution 
-// component.
-//
-// @@@@ ISSUES @@@@
+// If the execution log matches the golden log this test compiled
+// and executed correctly.
 
 #include <systemc>
 
 SC_MODULE(M) {
     SC_CTOR(M) {
         SC_THREAD(run);
+        SC_THREAD(controller);
     }
 
     void run()
     {
+        run_process = sc_core::sc_get_current_process_handle();
         for (;; ) {
 	    try {
 		sc_core::wait(1, sc_core::SC_NS);
@@ -29,12 +29,20 @@ SC_MODULE(M) {
 	    }
 	}
     }
+    void controller()
+    {
+        sc_core::wait(10, sc_core::SC_NS);
+	run_process.reset();
+        sc_core::wait(10, sc_core::SC_NS);
+	run_process.kill();
+    }
+    sc_core::sc_process_handle run_process;
 };
 
 int sc_main( int argc, char* argv[] ) { 
 
     M m("m");
-    sc_core::sc_start(10, sc_core::SC_NS);
+    sc_core::sc_start();
 
     std::cout << "program completed" << std::endl;
     return 0;
--- tests/systemc/misc/user_guide/ieee1666_2022/5.6.8/5.6.8.cpp
+++ tests/systemc/misc/user_guide/ieee1666_2022/5.6.8/5.6.8.cpp
@@ -1,21 +1,19 @@
-// NEEDS FURTHER INVESTIGATION
+// COMPLETE EXAMPLE
 //
-// If the execution log matches the golden log the compilation and normal execcution
-// are validated. But sc_is_unwinding(() being true is not being tested.
-//
-// @@@@ ISSUES @@@@
+// If the execution log matches the golden log this example compiles and
+// exectues correctly.
 
 #define SC_INCLUDE_DYNAMIC_PROCESSES
 #include <systemc>
 
 struct wait_on_exit {
-    ~wait_on_exit() {
+    ~wait_on_exit() noexcept(false) {
         if ( !sc_core::sc_is_unwinding() ) {
-	    std::cout << "not unwinding" << std::endl;
+	    std::cout << sc_core::sc_time_stamp() << " waiting in ~wait_on_exit()" << std::endl;
 	    wait(10.0, sc_core::SC_NS);
 	}
 	else {
-	    std::cout << "unwinding" << std::endl;
+	    std::cout << sc_core::sc_time_stamp() << " unwinding in ~wait_on_exit()" << std::endl;
 	}
     }
 };
@@ -23,27 +21,41 @@ struct wait_on_exit {
 SC_MODULE(M) 
 {
     SC_CTOR(M) {
+	SC_THREAD(controller);
 	SC_THREAD(some_process);
     }
+
+    void controller()
+    {
+        wait(25.0, sc_core::SC_NS);
+	some_process_handle.kill();
+    }
+
     void some_process()
     {
+        some_process_handle = sc_core::sc_get_current_process_handle();
 	while(true) {
 	    try {
 		wait_on_exit w;
 	    }
 	    catch( const sc_core::sc_unwind_exception& ) {
+	        std::cout << sc_core::sc_time_stamp() << " caught exception" << std::endl;
+		if ( !sc_core::sc_is_unwinding() ) {
+		    std::cout << "unwinding" << std::endl;
+		}
 		throw;
 	    }
 
 	}
     }
+    sc_core::sc_process_handle some_process_handle;
 };
 
 int sc_main( int argc, char* argv[] ) { 
 
     M m("m");
 
-    sc_start(11, sc_core::SC_NS);
+    sc_core::sc_start();
 
     std::cout << "program completed" << std::endl;
     return 0;
