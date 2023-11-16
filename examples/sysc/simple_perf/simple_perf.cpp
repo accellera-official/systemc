466021ed1592aec5990c86235f073502afc6d1e8
Philipp A. Hartmann <pah@apple.com>
2023-11-16 14:15:51 +0100

    SC_HAS_PROCESS: remove remaining uses
    Signed-off-by: Philipp A. Hartmann <pah@apple.com>

--- examples/sysc/simple_perf/simple_perf.cpp
+++ examples/sysc/simple_perf/simple_perf.cpp
@@ -211,8 +211,6 @@ class consumer : public sc_module
   public:
     sc_port<read_if> in;
 
-    SC_HAS_PROCESS(consumer);
-
     consumer(sc_module_name name) : sc_module(name)
     {
       SC_THREAD(main);
