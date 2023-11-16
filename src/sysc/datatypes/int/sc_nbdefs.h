7ac897ad2e408725ec937e09017f23546a0d4a9c
Thilo Voertler <thilo.voertler@coseda-tech.com>
2023-11-16 11:01:24 +0900

    Fix formatting, remove platform dependencies

--- src/sysc/datatypes/int/sc_nbdefs.h
+++ src/sysc/datatypes/int/sc_nbdefs.h
@@ -68,12 +68,8 @@
 #include "sysc/kernel/sc_cmnhdr.h"
 
 #include <climits>
+#include <cstdint>
 
-#if defined(__sun) || defined(__sun__)
-#  include <inttypes.h>
-#elif !defined(WIN32) && !defined(_WIN32)
-#  include <stdint.h>
-#endif
 
 #include "sysc/utils/sc_iostream.h"
 #include "sysc/utils/sc_string.h"       // For sc_numrep
@@ -215,32 +211,14 @@ typedef unsigned int sc_digit;        // type holding "digits" in big values.
 // store x bits. x is a positive number.
 #define DIV_CEIL(x) DIV_CEIL2(x, BITS_PER_DIGIT)
 
-// Support for the long long type. This type is not in the standard
-// but is usually supported by compilers.
-// Note: we cannot use (u)int64_t here as it causes ambiguities in the
-// SystemC datatype definition
-#ifndef _WIN32
-#   if defined(__x86_64__) || defined(__aarch64__)
-        typedef long long          int64;
-        typedef unsigned long long uint64;
-#   else
-        typedef int64_t            int64;
-        typedef uint64_t           uint64;
-#   endif
-#else
-    typedef __int64            int64;
-    typedef unsigned __int64   uint64;
-#endif
 
-#if !defined(_WIN32) || defined(__MINGW32__)
-    static const uint64 UINT64_ZERO   = 0ULL;
-    static const uint64 UINT64_ONE    = 1ULL;
-    static const uint64 UINT64_32ONES = 0x00000000ffffffffULL;
-#else
-    static const uint64 UINT64_ZERO   = 0i64;
-    static const uint64 UINT64_ONE    = 1i64;
-    static const uint64 UINT64_32ONES = 0x00000000ffffffffi64;
-#endif
+using int64  = long long;
+using uint64 = unsigned long long;
+
+static const uint64 UINT64_ZERO   = 0ULL;
+static const uint64 UINT64_ONE    = 1ULL;
+static const uint64 UINT64_32ONES = 0x00000000ffffffffULL;
+
 #if BITS_PER_DIGIT < 32
     typedef unsigned int sc_carry;    // type of carry temporaries.
 #else
@@ -269,11 +247,7 @@ typedef unsigned int sc_digit;        // type holding "digits" in big values.
 // Above, BITS_PER_X is mainly used for sc_signed, and BITS_PER_UX is
 // mainly used for sc_unsigned.
 
-#if defined( WIN32 ) || defined( __SUNPRO_CC ) || defined( __HP_aCC )
-typedef unsigned long fmtflags;
-#else
-typedef ::std::ios::fmtflags fmtflags;
-#endif
+using fmtflags = ::std::ios::fmtflags;
 
 extern const small_type NB_DEFAULT_BASE ;
 
