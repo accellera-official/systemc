f2b0cf040aee65fd064cf2a422b7790f3867d08f
Eyck Jentzsch <eyck@minres.com>
2025-09-30 19:43:21 +0200

    updates sc_log_types.h license header

--- src/sysc/log/sc_log_types.h
+++ src/sysc/log/sc_log_types.h
@@ -1,18 +1,24 @@
-/*******************************************************************************
- * Copyright 2016-2022 MINRES Technologies GmbH
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
+/*****************************************************************************
+  Licensed to Accellera Systems Initiative Inc. (Accellera) under one or
+  more contributor license agreements.  See the NOTICE file distributed
+  with this work for additional information regarding copyright ownership.
+  Accellera licenses this file to you under the Apache License, Version 2.0
+  (the "License"); you may not use this file except in compliance with the
+  License.  You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+  implied.  See the License for the specific language governing
+  permissions and limitations under the License.
+ *****************************************************************************/
+/*****************************************************************************
+  sc_log.h --SystemC logging functions.
+  Original Author: Eyck Jentzsch, MINRES Technologies GmbH
+                   Mark Burton, Qualcomm Technologies, Inc.
+                   
+  CHANGE LOG AT THE END OF THE FILE
+ *****************************************************************************/
 
 #ifndef _SC_LOG_TYPES_H_
 #define _SC_LOG_TYPES_H_
