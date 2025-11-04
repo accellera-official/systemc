559f6adb80a21f00c066f687738f10a351856057
Jerome Haxhiaj <jhaxhiaj@qti.qualcomm.com>
2025-11-04 22:07:52 +0100

    CI: Add msys2 UCRT64 and CLANGARM64 targets
    This commit adds two new platform for cmake and regressions tests.
    - MSYS2 clangarm64 (Windows on ARM64 runners)
    - MSYS2 UCRT64 (Windows Latest runner)
    
    Signed-off-by: Jerome Haxhiaj <jhaxhiaj@qti.qualcomm.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -30,6 +30,12 @@ case "$SYSTEMC_CI_TARGET" in
     BUILD_SHARED_LIBRARY=true
     BUILD_REGRESSIONS=true
     ;;
+  gcc-static-regression)
+    CC=gcc
+    CXX=g++
+    BUILD_SHARED_LIBRARY=false
+    BUILD_REGRESSIONS=true
+    ;;
   clang-shared)
     CC=clang
     CXX=clang++
@@ -46,6 +52,12 @@ case "$SYSTEMC_CI_TARGET" in
     CC=clang
     CXX=clang++
     BUILD_SHARED_LIBRARY=true
+    BUILD_REGRESSIONS=true
+    ;;
+  clang-static-regression)
+    CC=clang
+    CXX=clang++
+    BUILD_SHARED_LIBRARY=false
     BUILD_REGRESSIONS=true
     ;;
   clang-shared-regression-asan)
@@ -75,16 +87,20 @@ case "$SYSTEMC_CI_TARGET" in
     ;;
 esac
 
-cd $SYSTEMC_SRC_PATH
-cmake -B BUILD/RELEASE-${SYSTEMC_CI_TARGET}/BUILD \
-      -DCMAKE_INSTALL_PREFIX=${SYSTEMC_SRC_PATH}/BUILD/${SYSTEMC_CI_TARGET} \
+
+cd "$SYSTEMC_SRC_PATH"
+cmake -B "BUILD/RELEASE-${SYSTEMC_CI_TARGET}/BUILD" \
+      -DCMAKE_INSTALL_PREFIX="${SYSTEMC_SRC_PATH}/BUILD/${SYSTEMC_CI_TARGET}" \
       -DCMAKE_CXX_FLAGS="$CXX_FLAGS" \
-      -DCMAKE_C_COMPILER=$CC \
-      -DCMAKE_CXX_COMPILER=$CXX \
-      -DENABLE_REGRESSION=$BUILD_REGRESSIONS \
-      -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBRARY .
-cmake --build BUILD/RELEASE-${SYSTEMC_CI_TARGET}/BUILD/ --parallel
-cmake --install BUILD/RELEASE-${SYSTEMC_CI_TARGET}/BUILD/
-make -j `getconf _NPROCESSORS_ONLN` -C BUILD/RELEASE-${SYSTEMC_CI_TARGET}/BUILD/ check
+      -DCMAKE_C_COMPILER="$CC" \
+      -DCMAKE_CXX_COMPILER="$CXX" \
+      -DENABLE_REGRESSION="$BUILD_REGRESSIONS" \
+      -DBUILD_SHARED_LIBS="$BUILD_SHARED_LIBRARY" .
+cmake --build "BUILD/RELEASE-${SYSTEMC_CI_TARGET}/BUILD/" --parallel
+cmake --install "BUILD/RELEASE-${SYSTEMC_CI_TARGET}/BUILD/"
+
+if [[ "$BUILD_REGRESSIONS" == "true" ]]; then
+  cmake --build "BUILD/RELEASE-${SYSTEMC_CI_TARGET}/BUILD/" --parallel "$(getconf _NPROCESSORS_ONLN)" --target check
+fi
 
 exit 0
